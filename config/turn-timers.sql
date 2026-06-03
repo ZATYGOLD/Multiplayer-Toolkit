4fd483f46ec8841cc4014ef94e792b25848c0ad3
ZATYGOLD <30324251+ZATYGOLD@users.noreply.github.com>
2026-06-03 22:40:05 +0800

    revert

--- config/turn-timers.sql
+++
@@ -1,8 +0,0 @@
--- config/turn-timers.sql
--- Author: Zatygold
-
---*******************************************************
---************** COMPETITIVE TIMER SETTINGS *************
---*******************************************************
-INSERT INTO TurnTimers (Domain, TurnTimerType, Name, Description, SortIndex)
-VALUES ('StandardTurnTimers', 'MPT_TURNTIMER_COMPETITIVE', 'LOC_MPT_TURNTIMER_COMPETITIVE', 'LOC_MPT_TURNTIMER_COMPETITIVE_DESC', 40);
